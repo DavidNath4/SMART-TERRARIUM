52c84ee0b42a2137f71531d61a55eb6cb8ee58c1
nathan <davidnathansitorus@gmail.com>
2023-04-14 13:38:58 +0700

    4/14/2023

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cookieParser = require("cookie-parser");
 const ROUTER = require("./router");
 require('./API/API_SCHEDULE/connection/subscribe');
 const expbs = require("express-handlebars");
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -16,14 +17,18 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-app.engine('handlebars', expbs.engine());
+app.engine('handlebars', expbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views/mainLayout')
+}));
 app.set('view engine', 'hbs');
-app.get('/', async (req, res) => {
-    res.render('index');
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home Page' });
 });
 
-app.get('/about', async (req, res) => {
-    res.render('about');
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About Page' });
 });
 
 app.use("/", ROUTER);
